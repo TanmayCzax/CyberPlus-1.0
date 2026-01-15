f30f586bc9bf26994639230d97dc7bb882ca7ea7
Czax <pkcisf@gmail.com>
2026-01-15 12:26:52 +0530

    Update script.js

--- Cyber-Programming-language-Alpha-main/script.js
+++ Cyber-Programming-language-Alpha-main/script.js
@@ -1,18 +1,1 @@
 
-function goToRepo() {
-  window.location.href = "https://github.com/TanmayCzax/Cyber-Programming-language-Alpha";
-}
-
-function scrollToSection(id) {
-  const section = document.getElementById(id);
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const yearEl = document.getElementById("year");
-  if (yearEl) {
-    yearEl.textContent = new Date().getFullYear();
-  }
-});
